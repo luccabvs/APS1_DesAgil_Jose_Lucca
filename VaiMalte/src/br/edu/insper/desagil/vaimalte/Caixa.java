650c665881df37ad2b3dea92d38f48d35935f90c
josermf2 <josermf2@al.insper.edu.br>
2021-03-21 02:38:36 -0300

    Classe Produtos Feita

--- VaiMalte/src/br/edu/insper/desagil/vaimalte/Caixa.java
+++ VaiMalte/src/br/edu/insper/desagil/vaimalte/Caixa.java
@@ -14,29 +14,23 @@ public class Caixa {
 	}
 	
 	public void adiciona_desconto(Produto produto, int desconto) {
-		
 		if ( desconto > 0 && desconto < 100 ) {
-			
 			this.descontos.put(produto.getCodigo(), desconto);
 		}
 	}
 	
 	public double total(Carrinho carrinho){
 		double total_carrinho = 0.0;
-		for (Pedido pedido :carrinho.getPedidos()) {
-			if (descontos.containsKey(pedido.getProduto().getCodigo())) {
-				total_carrinho  += (1 - descontos.get(pedido.getProduto().getCodigo())/100) * pedido.getProduto().getPreco();
+		for (Pedido pedido : carrinho.getPedidos()){
+			if (this.descontos.containsKey(pedido.getProduto().getCodigo())) {
+				double desconto = this.descontos.get(pedido.getProduto().getCodigo());
+				total_carrinho  += pedido.getQuantidade() * pedido.getProduto().getPreco() * (1 - ((desconto)/100));
 			}
-			
 			else {
-				total_carrinho += pedido.getProduto().getPreco();
+				total_carrinho += pedido.getQuantidade() * pedido.getProduto().getPreco();
 			}
 		}
-		
 		return total_carrinho;
 	}
 	
 }
-	
-	
-
--- VaiMalte/src/br/edu/insper/desagil/vaimalte/Testador.java
+++ VaiMalte/src/br/edu/insper/desagil/vaimalte/Testador.java
@@ -2,16 +2,49 @@ package br.edu.insper.desagil.vaimalte;
 
 public class Testador {
 	public boolean testeA() {
-	    return false;
+		Carrinho carrinho_A = new Carrinho(); 
+		
+		Caixa caixa_A = new Caixa();
+	    
+		if(caixa_A.total(carrinho_A) == 0.0) {
+			return true;
+		}
+		
+		return false;	
 	}
 
 	public boolean testeB() {
-	    return false;
+		Carrinho carrinho_B = new Carrinho(); 
+		
+		Produto Litrao_do_Quata = new Produto(33, "Litrao_do_Quata", 13.99);
+				
+		carrinho_B.incrementa(Litrao_do_Quata);
+		
+		Caixa caixa_B = new Caixa();
+		
+		if(caixa_B.total(carrinho_B) == 13.99) {
+			return true;
+		}
+		
+		return false;	
 	}
 
 	public boolean testeC() {
-	    return false;
-	}
+		Carrinho carrinho_C = new Carrinho(); 
+		
+		Produto Litrao_do_Sujus = new Produto(33, "Litrao_do_Sujus", 15.00);
+				
+		carrinho_C.incrementa(Litrao_do_Sujus);
+		
+		Caixa caixa_C = new Caixa();
+
+		caixa_C.adiciona_desconto(Litrao_do_Sujus, 30);
+		
+		if(caixa_C.total(carrinho_C) == 10.50) {
+			return true;
+		}
+		
+		return false;		}
 
 	public boolean testeD() {
 	    return false;
